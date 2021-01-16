3c1fedf4c83bb1bc9e5926dd3c45924a19df19d4
Administrator <abc_cn@163.com>
2021-01-16 11:59:02 +0800

    First Commit

--- product-view-service-feign/src/main/java/cn/how2j/springcloud/web/ProductController.java
+++ product-view-service-feign/src/main/java/cn/how2j/springcloud/web/ProductController.java
@@ -3,6 +3,7 @@ package cn.how2j.springcloud.web;
 import cn.how2j.springcloud.pojo.Product;
 import cn.how2j.springcloud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,14 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
+    @Value("${version}")
+    String version;
+
     @RequestMapping("/products")
     public Object products(Model m){
         List<Product> ps = productService.listProducts();
         m.addAttribute("ps", ps);
+        m.addAttribute("version", version);
         return "products";
     }
 }
